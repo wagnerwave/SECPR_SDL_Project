76bd044f0ace393fcae04bbccbdd0108be4789d5
wagnerwave <alexandre1.wagner@epitech.eu>
2021-09-21 16:01:47 +0200

    remove useless code

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,6 @@ import { useHistory } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import jwt from "jsonwebtoken";
 
-import GoogleSignIn from './GoogleSignIn';
-import GithubSignIn from './GithubSignIn';
-import TwitchSignIn from './TwitchSignIn';
-
 const Login = () => {
     const SecretToken = "YXJtYW5kb2JvbmQ=";
     const history = useHistory();
@@ -61,10 +57,6 @@ const Login = () => {
                     <p>You don't have an account ? </p>
                     <a href="/Register">Register page</a>
                 </form>
-                <hr></hr>
-                <GoogleSignIn />
-                <GithubSignIn />
-                <TwitchSignIn />
             </div>
         </Fragment>
     );
